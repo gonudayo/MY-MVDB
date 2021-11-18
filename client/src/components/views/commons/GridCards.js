abb0ce94e1f1bc89daef5e40e022a4dfb25ef90c
gonudayo <kgw010119@gmail.com>
2021-11-18 14:41:13 +0000

    fix: Grid 표시 개수 조정

--- client/src/components/views/commons/GridCards.js
+++ client/src/components/views/commons/GridCards.js
@@ -4,19 +4,19 @@ import { Col } from 'antd';
 function GridCards(props) {
 	if(props.landingPage) {
 		return (
-			<Col xxl={2} xl={4} lg={6} md={8} sm={10} xs={12}>
-				<div style={{ width: '189.0px', height: '283.5px', position: 'relative'}}>
+			<Col xxl={3} xl={4} lg={6} md={8} sm={10} xs={12}>
+				<div style={{ width: '189.0px', height: '283.5px', position: 'relative', padding: '10px'}}>
 					<a href={`/movie/${props.movieId}`}>
-						<img style={{ width: '100%', height: 'auto', padding: '10px' }}src={props.image} alt={props.movieName} />
+						<img style={{ width: '100%', height: 'auto'}}src={props.image} alt={props.movieName} />
 					</a>
 				</div>
 			</Col>
 		);
 	} else {
 		return (
-			<Col xxl={2} xl={4} lg={6} md={8} sm={10} xs={12}>
-				<div style={{ position: 'relative' }}>
-					<img style={{ width: '190px', height: 'auto', padding: '10px' }}src={props.image} alt={props.charactermame} />
+			<Col xxl={3} xl={4} lg={6} md={8} sm={10} xs={12}>
+				<div style={{ width: '190px', position: 'relative', padding: '10px' }}>
+					<img style={{ width: '100%', height: 'auto' }}src={props.image} alt={props.charactermame} />
 				</div>
 			</Col>
 		);
